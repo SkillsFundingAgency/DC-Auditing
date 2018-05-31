4c81805e3ce135260883395dfc6658a6d494a189
John Youren <john.youren@gmail.com>
2018-05-31 09:49:51 +0100

    Added constructor for auditing persistence service config

--- src/ESFA.DC.Auditing.Persistence.Service/AudtingPersistenceServiceConfig.cs
+++ src/ESFA.DC.Auditing.Persistence.Service/AudtingPersistenceServiceConfig.cs
@@ -4,6 +4,11 @@ namespace ESFA.DC.Auditing.Persistence.Service
 {
     public sealed class AudtingPersistenceServiceConfig : IAuditingPersistenceServiceConfig
     {
+        public AudtingPersistenceServiceConfig(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
         public string ConnectionString { get; }
     }
 }
